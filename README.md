28dbea53ad0c0c03426a954697d846e51bec9c64
SummerLv <lv.summer@qq.com>
2022-03-22 14:30:12 +0800

    2022年3月22日14点30分搭建基本框架

---
+++ README.md
@@ -0,0 +1,10 @@
+# 参考
+[教学视频参考](https://www.bilibili.com/video/BV1gJ411p7xC?p=25&spm_id_from=pageDriver)
+
+[go中文网](https://studygolang.com/dl)
+
+[golang博客](https://www.liwenzhou.com/)
+
+[go web 框架 Gin](https://gin-gonic.com/)
+
+[golang orm 框架](https://gorm.io/docs/index.html)
--- main.go
+++ main.go
@@ -5,13 +5,48 @@ import (
 	"net/http"
 )
 
+type Todo struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
 func main() {
+	// 创建数据库 TODO
+
 	r := gin.Default()
 	// 告诉 gin 去指定目录下寻找静态文件
 	r.LoadHTMLGlob("templates/*")
+	r.Static("/static", "static")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 	// 默认 8080 端口
 	r.Run()
+
+	// api v1
+	v1Group := r.Group("v1")
+	{
+		// 添加一个代办
+		v1Group.POST("/todo", func(c *gin.Context) {
+
+		})
+		// 删除某一个代办
+		v1Group.DELETE("/todo/:id", func(c *gin.Context) {
+
+		})
+		// 修改某一个代办
+		v1Group.PUT("/todo/:id", func(c *gin.Context) {
+
+		})
+		// 查询所有代办
+		v1Group.GET("/todo", func(c *gin.Context) {
+
+		})
+		// 查询某一个代办
+		v1Group.GET("/todo/:id", func(c *gin.Context) {
+
+		})
+	}
+
 }
